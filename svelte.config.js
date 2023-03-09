80575c95986d73282e2dbd91760f7196fdaa31dc
Macbook 12 <jonathantawfik@gmail.com>
2023-03-09 17:47:11 +0800

    fixed netlify dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
 import adapter from '@sveltejs/adapter-netlify';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			edge:true
-		})
-	}
+ 
+export default {
+  kit: {
+    adapter: adapter({
+      // will create a Netlify Edge Function using Deno-based
+      // rather than using standard Node-based functions
+      edge: true
+    })
+  }
 };
-
-export default config;
